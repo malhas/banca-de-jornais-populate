3b50e27a5c26eebf6b1ea943b1009757c473632d
malhas <luismalheiro1986@gmail.com>
2023-07-20 20:24:34 +0100

    Updated data to be added to db to include item_id

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import requests
 from pymongo import MongoClient
 import certifi
 from dotenv import load_dotenv
+
 load_dotenv()
 
 MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD")
 BASE_URL = "https://services.sapo.pt/News/NewsStand/"
-ENDPOINTS = ["National", "Sport", "Economy",
-             "International", "Regional", "Magazine"]
+ENDPOINTS = ["National", "Sport", "Economy", "International", "Regional", "Magazine"]
 
 
 def create_object() -> list:
@@ -22,8 +22,7 @@ def create_object() -> list:
     """
     capas = []
     for endpoint in ENDPOINTS:
-        document = requests.get(
-            BASE_URL + endpoint).text
+        document = requests.get(BASE_URL + endpoint).text
         dom_dict = xmltodict.parse(document)
         category = dom_dict["newsstand"]["name"]
         for element in dom_dict["newsstand"]["bj_editionsgroup"]["bj_related_image"]:
@@ -32,6 +31,7 @@ def create_object() -> list:
                 "link": element["link"],
                 "image_url": element["image_url"],
                 "publish_date": element["publish_date"],
+                "item_id": element["id"],
                 "category": category,
             }
             capas.append(capa)
@@ -53,7 +53,7 @@ def populate_db(items: list):
     uri = f"mongodb+srv://admin:{MONGODB_PASSWORD}@cluster0.3goysx3.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(uri, tlsCAFile=certifi.where())
     try:
-        client.admin.command('ping')
+        client.admin.command("ping")
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
@@ -61,8 +61,7 @@ def populate_db(items: list):
     db = client["capasjornais"]
     collection = db["Capas"]
     for item in items:
-        dbitem = collection.find_one(
-            {"name": item["name"], "publish_date": item["publish_date"]})
+        dbitem = collection.find_one({"name": item["name"], "publish_date": item["publish_date"]})
         if not dbitem:
             collection.insert_one(item)
             count += 1
